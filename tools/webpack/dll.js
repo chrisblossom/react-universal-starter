a95088c2d0e92ecbd7b998b6f6c9721511cd4cb0
Chris Blossom <cbloss@gmail.com>
2017-06-12 16:16:18 -0600

    hard-source-webpack-plugin for dll

--- tools/webpack/dll.js
+++ tools/webpack/dll.js
@@ -6,10 +6,11 @@
 
 const path = require('path');
 const webpack = require('webpack');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 
 const config = require('../config/env');
 
-const { buildPath, publicPath, srcPath } = config.paths;
+const { buildPath, publicPath, rootPath, srcPath } = config.paths;
 
 const dll = {
     context: srcPath,
@@ -49,6 +50,38 @@ const dll = {
         new webpack.DllPlugin({
             path: path.resolve(buildPath, '[name]-manifest.dll.json'),
             name: '[name]',
+        }),
+
+        new HardSourceWebpackPlugin({
+            // Either an absolute path or relative to output.path.
+            cacheDirectory: path.resolve(
+                rootPath,
+                '.cache/hard-source/dll/[confighash]'
+            ),
+            // Either an absolute path or relative to output.path. Sets webpack's
+            // recordsPath if not already set.
+            recordsPath: path.resolve(
+                rootPath,
+                '.cache/hard-source/dll/[confighash]/records.json'
+            ),
+            // Either a string value or function that returns a string value.
+            configHash: function(webpackConfig) {
+                // Build a string value used by HardSource to determine which cache to
+                // use if [confighash] is in cacheDirectory or if the cache should be
+                // replaced if [confighash] does not appear in cacheDirectory.
+                //
+                // node-object-hash on npm can be used to build this.
+                return require('node-object-hash')({ sort: false }).hash(
+                    webpackConfig
+                );
+            },
+            // This field determines when to throw away the whole cache if for
+            // example npm modules were updated.
+            environmentHash: {
+                root: process.cwd(),
+                directories: ['node_modules'],
+                files: ['package.json'],
+            },
         }),
     ],
 };
