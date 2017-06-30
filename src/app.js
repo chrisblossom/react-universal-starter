be51ed249ec19ee1eff6be1c07936e8e69c3fa8b
Chris Blossom <cbloss@gmail.com>
2017-06-30 12:38:15 -0700

    use chunkNames

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const LoadableMyComponent = universal(
     () => import(/* webpackChunkName: 'app2' */ './app2'),
     {
         resolve: () => require.resolveWeak('./app2'),
+        chunkName: 'app2'
     },
 );
 
