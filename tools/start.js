0147135d452479a951e23e529e00219aeb1348d6
Chris Blossom <cbloss@gmail.com>
2017-07-11 11:25:53 -0700

    remove packages preventing normal start

--- tools/start.js
+++ tools/start.js
@@ -1,21 +1,13 @@
 /* @flow */
 
-const log = require('./utils/log');
 const run = require('./run');
 const clean = require('./clean');
 const copy = require('./copy');
 const bundleDll = require('./bundle_dll');
-const RedisServer = require('redis-server');
 const watchServer = require('./watch_server');
 const watchFrontend = require('./watch_frontend');
 
 async function start() {
-    const redis = new RedisServer();
-
-    await redis.open().catch(error => {
-        log.error('redis:', error.message);
-    });
-
     await run(clean);
     await run(copy);
     await run(bundleDll);
